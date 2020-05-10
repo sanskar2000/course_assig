956d2d123d8685fcc0fe2da74d371819d7c0735b
sanskar2000 <59883255+sanskar2000@users.noreply.github.com>
2020-05-10 19:01:52 +0530

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ for (var i = 0; i < names.length; i++) {
 
   
   if (firstLetter === 'j') {
-    console.log("Goodbye "+names[i]);
+    console.log("Good Bye "+names[i]);
   } else {
     console.log("Hello "+names[i]);
   }
